b2f41877eb298f2b5d08ba3c757cfffbdeb8eec9
Sebastian Sommer <s.sommer@netzbewegung.com>
2012-04-25 16:48:49 +0200

    Added: pr function for php

---
+++ pr.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Pr - The ultimate debug function
+ * 
+ * @author Sebastian Sommer <mail@sebastian-sommer.com>
+ * @author Steffen Mächtel <mail@meffen.de>
+ */
+
+function pr($value, $varDump = false)
+{
+    if ($varDump === false)
+    {
+        $message = print_r($value, true);
+    }
+    else
+    {
+        $message = var_dump($value);
+    }
+
+    echo '<pre style="margin: 10px; padding: 10px; border: 1px solid red;">' . $message . '</pre>';
+
+    if (count(ob_list_handlers()))
+    {
+        ob_flush();
+    }
+}
