f68f16f57bfe0b207aa4ada73b49ae7fbd5b976c
Daniel Wiegand <d.wiegand@firstsquare.de>
2012-12-14 16:02:15 +0100

    - fixed coding guidelines

--- pr.php
+++ pr.php
@@ -1,26 +1,21 @@
 <?php
+
 /**
  * pr - The ultimate debug function
  * 
  * @author Sebastian Sommer <mail@sebastian-sommer.com>
  * @author Steffen Mächtel <mail@meffen.de>
  */
-
-function pr($value, $varDump = false)
-{
-    if ($varDump === false)
-    {
+function pr($value, $varDump = false) {
+    if ($varDump === false) {
         $message = print_r($value, true);
-    }
-    else
-    {
+    } else {
         $message = var_dump($value);
     }
 
     echo '<pre style="margin: 10px; padding: 10px; border: 1px solid red;">' . $message . '</pre>';
 
-    if (count(ob_list_handlers()))
-    {
+    if (count(ob_list_handlers())) {
         ob_flush();
     }
 }
